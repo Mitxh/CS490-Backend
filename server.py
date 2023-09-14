b8a6969fa6498407fd9325fe3317131f374cf02b
Mitchell <mdb4@njit.edu>
2023-09-14 04:11:28 -0400

    returns {"print":"["200", "Hello World"]}, works but only once

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+# from healthcheck import HealthCheck
+app=Flask(__name__)
+# health=HealthCheck()
+# def test():
+#    return True, "Hello World"
+# health.add_check(test)
+# app.add_url_rule('/healthcheck', 'healthcheck', view_func=lamda: health.run())
+
+@app.route('/healthcheck')
+def heath_check():
+   return {"print":["200", "Hello World"]}
+if __name__ == "__main__":
+   app.run(debug=False)
