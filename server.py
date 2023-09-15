3232b0572feb101c456f44dbe1bcf948893b0583
Mitchell <mdb4@njit.edu>
2023-09-14 21:14:42 -0400

    Commit for netlify

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 # from healthcheck import HealthCheck
 app=Flask(__name__)
+CORS(app)
 # health=HealthCheck()
 # def test():
 #    return True, "Hello World"
