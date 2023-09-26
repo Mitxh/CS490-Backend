8663618577c72e6d1a7c0486d8e94c700c93e95b
Mitchell <mdb4@njit.edu>
2023-09-26 16:36:07 -0400

    movies and actors app route working

--- main.py
+++ main.py
@@ -4,25 +4,35 @@ import mysql.connector
 # from healthcheck import HealthCheck
 app = Flask(__name__)
 CORS(app)
-# health=HealthCheck()
-# def test():
-#    return True, "Hello World"
-# health.add_check(test)
-# app.add_url_rule('/healthcheck', 'healthcheck', view_func=lamda: health.run())
-cnx = mysql.connector.connect(
-    user='root', password='Mb235957', host='localhost', database='sakila')
 
+@app.route('/movies')
+def movie_list():
+  cnx = mysql.connector.connect(user='root', password='Mb235957', host='localhost', database='sakila')
+  cursor = cnx.cursor()
+  query = 'SELECT film.film_id, film.title AS title, COUNT(*) AS count, film.description FROM rental INNER JOIN inventory ON inventory.inventory_id=rental.inventory_id INNER JOIN film ON inventory.film_id=film.film_id GROUP BY film_id, title ORDER BY count DESC LIMIT 5;'
+  cursor.execute(query)
+  results = cursor.fetchall()
+  cursor.close()
+  cnx.close()
+  results = [list(row) for row in results]
+  for i in range(len(results)):
+    results[i][1] = results[i][1].title()
+  return jsonify(results)
 
-@app.route('/')
-def heath_check():
-    #  return jsonify(status=200, print="Hello World")
-    cursor = cnx.cursor()
-    query = 'SELECT film.film_id, film.title AS title, COUNT(*) AS count FROM rental INNER JOIN inventory ON inventory.inventory_id=rental.inventory_id INNER JOIN film ON inventory.film_id=film.film_id GROUP BY film_id, title ORDER BY count DESC LIMIT 5;'
-    cursor.execute(query)
-    results = cursor.fetchall()
-
-    return jsonify(results)
-
+@app.route('/actors')
+def actor_list():
+  cnx = mysql.connector.connect(user='root', password='Mb235957', host='localhost', database='sakila')
+  cursor = cnx.cursor()
+  query = 'SELECT actor.actor_id, actor.first_name AS "First", actor.last_name AS "Last", COUNT(film_id) AS "Film Count" FROM actor INNER JOIN film_actor ON film_actor.actor_id=actor.actor_id GROUP BY actor.actor_id ORDER BY COUNT(film_id) DESC LIMIT 5;'
+  cursor.execute(query)
+  results = cursor.fetchall()
+  cursor.close()
+  cnx.close()
+  results = [list(row) for row in results]
+  for i in range(len(results)):
+    results[i][1] = results[i][1].title()
+    results[i][2] = results[i][2].title()
+  return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
